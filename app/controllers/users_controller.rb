48020b888a34745ba0078cdebda03ed61f348e5d
EC2 Default User <ec2-user@ip-172-31-2-3.ap-northeast-1.compute.internal>
2020-10-05 12:06:09 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  
   def index
+    @users = User.all.page(params[:page]).per(25)
   end
   
   def show
@@ -20,9 +25,49 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    
+  def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    #   # 更新に成功した場合を扱う。
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    flash[:success] = "#{current_user.name}は#{user.name}を逃した。バイバイ！#{user.name}"
+     redirect_to users_url
+  end
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
 end
